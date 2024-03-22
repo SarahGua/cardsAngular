3320633c41a4b5ca1e448b57d32a5851bfe39849
SarahGuarneri <sarahguarneri97@icloud.com>
2024-03-22 15:35:18 +0100

    app complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cardsAngular';
+  posts = [
+    { 
+      title: 'Neat Tree', 
+      imageUrl : 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { 
+      title: 'Snowy Mountain', 
+      imageUrl : 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'I saw this mountain today'
+    },
+    { 
+      title: 'Mountain biking', 
+      imageUrl : 'assets/biking.jpeg',
+      username: 'biking',
+      content: 'I did some biking today'
+    }
+  ]
 }
